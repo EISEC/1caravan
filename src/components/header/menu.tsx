e66cff55b3fc37c06b323550692dd8997f6905c1
eisec <info@eisec.ru>
2023-02-22 23:31:20 +0300

    add showMore button

--- src/components/header/menu.tsx
+++ src/components/header/menu.tsx
@@ -4,26 +4,65 @@ import cl from './menu.module.css'
 
 const Menu = () => {
 
+
     return (
         <header className={cl.menu}>
-            <Link className={cl.btn2} href={'/'}>
-                Главная
-            </Link>
-            <Link className={cl.btn2} href={'/catalog'}>
-                Каталог
-            </Link>
-            <Link className={cl.btn2} href={'/pod-zakaz'}>
-                Под заказ
-            </Link>
-            <Link className={cl.btn2} href={'/sold'}>
-                Проданные
-            </Link>
-            <Link className={cl.btn2} href={'/tavary'}>
-                Товары
-            </Link>
-            <Link className={cl.btn2} href={'/blog'}>
-                Блог
-            </Link>
+
+            {/*// @ts-ignore*/}
+            <nav itemscope="" itemtype="https://schema.org/SiteNavigationElement">
+                {/*// @ts-ignore*/}
+                <ul itemprop="about" itemscope="" itemtype="https://schema.org/ItemList">
+                    {/*// @ts-ignore*/}
+                    <li itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ItemList">
+                        {/*// @ts-ignore*/}
+                        <Link className={cl.btn2} href={'/'} itemprop="url">
+                            Главная
+                        </Link>
+                        <meta itemProp="name" content="Главная"/>
+                    </li>
+                    {/*// @ts-ignore*/}
+                    <li itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ItemList">
+                        {/*// @ts-ignore*/}
+                        <Link className={cl.btn2} href={'/catalog'} itemprop="url">
+                            Каталог
+                        </Link>
+                        <meta itemProp="name" content="Каталог"/>
+                    </li>
+                    {/*// @ts-ignore*/}
+                    <li itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ItemList">
+                        {/*// @ts-ignore*/}
+                        <Link className={cl.btn2} href={'/pod-zakaz'} itemprop="url">
+                            Под заказ
+                        </Link>
+                        <meta itemProp="name" content="Под заказ"/>
+                    </li>
+                    {/*// @ts-ignore*/}
+                    <li itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ItemList">
+                        {/*// @ts-ignore*/}
+                        <Link className={cl.btn2} href={'/sold'} itemprop="url">
+                            Проданые
+                        </Link>
+                        <meta itemProp="name" content="Проданные"/>
+                    </li>
+                    {/*// @ts-ignore*/}
+                    <li itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ItemList">
+                        {/*// @ts-ignore*/}
+                        <Link className={cl.btn2} href={'/tavary'} itemprop="url">
+                            Товары
+                        </Link>
+                        <meta itemProp="name" content="Товары"/>
+                    </li>
+                    {/*// @ts-ignore*/}
+                    <li itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ItemList">
+                        {/*// @ts-ignore*/}
+                        <Link className={cl.btn2} href={'/blog'} itemprop="url">
+                            Блог
+                        </Link>
+                        <meta itemProp="name" content="Блог"/>
+                    </li>
+                </ul>
+            </nav>
+
         </header>
     );
 };
