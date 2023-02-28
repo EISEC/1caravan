de4e64c0576587c18d95863029ee2573475b0618
eisec <info@eisec.ru>
2023-02-28 21:30:51 +0300

    Проделанно много работы, добавлены производители, завершен футер, переработаны посты

---
+++ src/pages/politika.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Menu from "@/components/header/menu";
+import Footer from "@/components/footer/footer";
+
+const Politika = () => {
+    return (
+        <>
+            <Menu/>
+            <main>
+
+            </main>
+            <Footer/>
+        </>
+    );
+};
+
+export default Politika;
