f99457b914173b999bc1f9720bcc277b6d2a338b
eisec <info@eisec.ru>
2023-06-02 21:51:31 +0300

    Redux Fix Key

--- src/pages/compare.tsx
+++ src/pages/compare.tsx
@@ -3,29 +3,48 @@ import CapHome from "@/components/home/capHome";
 import {clearComp} from "@/store/slice/compare";
 import Footer from "@/components/footer/footer";
 import {useAppDispatch, useAppSelector} from "@/store/store";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {log} from "util";
 
 const Compare = () => {
     const dispatch = useAppDispatch()
     const {compareList} = useAppSelector(state => state.compare)
+    const [caravans, setCaravans] = useState([])
+
+    useEffect(() => {
+        // @ts-ignore
+        let list =[]
+        compareList.forEach(async (item, index) =>  {
+            const caravan = await fetch(`https://1caravan.ru/wp-json/api/v2/doma/${item.slug}`)
+            const [post] = await caravan.json()
+            list.push(post)
+        })
+        // @ts-ignore
+        setCaravans(list)
+    },[])
     return (
         <>
             <Menu/>
             <main>
                 <CapHome/>
-                <button onClick={() => dispatch(clearComp())}>
-                    Очистить
-                </button>
-                <ul>
-                    {/*//@ts-ignore*/}
-                    {compareList.map(el => {
-                        return(
-                            <li key={el.slug}>
-                                {el.title}
-                            </li>
-                        )
-                    })}
-                </ul>
+                <div className="container mx-auto py-5 px-2">
+                    <ul>
+                        {/*//@ts-ignore*/}
+                        {caravans.map((el, idx) => {
+                            return (
+                                // @ts-ignore
+                                <li key={el.id}>
+                                    {/*// @ts-ignore*/}
+                                    {el.title}
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    <button onClick={() => dispatch(clearComp())} className="text-white bg-blue-700 px-5 py-2 rounded shadow-xl my-4">
+                        Очистить
+                    </button>
+                </div>
+                <button onClick={()=> console.log(caravans)}> Cgbcrf</button>
             </main>
             <Footer/>
         </>
