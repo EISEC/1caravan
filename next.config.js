26567c1498540a32254965ecdf87c6549e65c2d1
eisec <info@eisec.ru>
2023-07-29 23:33:03 +0300

    dopy

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  swcMinify:true,
-  optimizeFonts:true,
-  output: 'standalone',
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '1caravan.ru',
-      },
-    ],
-    minimumCacheTTL:1500000,
-  },
+    pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+    reactStrictMode: false,
+    swcMinify: true,
+    optimizeFonts: true,
+    output: 'standalone',
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '1caravan.ru',
+            },
+        ],
+        minimumCacheTTL: 1500000,
+    },
 }
 
 module.exports = nextConfig
